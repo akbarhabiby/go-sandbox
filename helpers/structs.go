60c09635db6e1863ea56ce7b4833b0ae3938cec1
Muhammad Akbar Habiby Khalid <akbarhabiby@icloud.com>
2021-05-17 18:21:00 +0700

    [Main] Added Structs Helper

---
+++ helpers/structs.go
@@ -0,0 +1,13 @@
+package structs
+
+import "fmt"
+
+// * Struct is a grouping of fields, like an Object in JavaScript
+type Person struct {
+	name string
+	age  int
+}
+
+func Struct() {
+	fmt.Println("Hello")
+}
